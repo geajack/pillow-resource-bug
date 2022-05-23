45f9360b2c1bda5eddb4aa59e0443cd94984e1fb
Millist, Jack <jack.millist@intel.com>
2022-05-23 12:08:14 +0100

    Simplify

--- font_demo/__main__.py
+++ font_demo/__main__.py
@@ -1,19 +1,15 @@
 import importlib.resources
-from PIL import Image, ImageFont, ImageDraw
+from PIL import ImageFont
 from PIL import _imagingft as core
 
-class Font:
 
-    def __init__(self, path):
-        self.font = core.getfont(
+if __name__ == "__main__":
+    with importlib.resources.path("font_demo", "OpenSans-Regular.ttf") as path:
+        font = core.getfont(
             str(path),
             30,
             0,
             "",
             layout_engine=ImageFont.Layout.BASIC
         )
-
-if __name__ == "__main__":
-    with importlib.resources.path("font_demo", "OpenSans-Regular.ttf") as path:
-        font = Font(path)
         
